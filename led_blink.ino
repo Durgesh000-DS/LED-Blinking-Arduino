1272a048a7b45b9254e720ac8695c6f8102ad1e4
Durgesh Savale <durgeshsavale2@gmail.com>
2026-04-01 16:55:47 +0530

    Add LED blinking functionality in led_blink.ino

---
+++ led_blink.ino
@@ -0,0 +1,16 @@
+// LED Blinking Project
+// Author: Durgesh Savale
+
+int ledPin = 13;  // LED connected to digital pin 13
+
+void setup() {
+  pinMode(ledPin, OUTPUT);  // Set pin as output
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); // Turn LED ON
+  delay(1000);                // Wait for 1 second
+
+  digitalWrite(ledPin, LOW);  // Turn LED OFF
+  delay(1000);                // Wait for 1 second
+}
